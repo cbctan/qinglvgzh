d7b942971ea2230dbb8fc232b1c654ac2fe6bec3
cbctan <43856000+cbctan@users.noreply.github.com>
2022-09-24 17:19:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,282 +1,234 @@
-from datetime import date, datetime
-import math
-from wechatpy import WeChatClient
-from wechatpy.client.api import WeChatMessage, WeChatTemplate
-import requests
-import os
 import random
-##########################农历转换##############################
-class Lunar(object):
-  #******************************************************************************
-  # 下面为阴历计算所需的数据,为节省存储空间,所以采用下面比较变态的存储方法.
-  #******************************************************************************
-  #数组g_lunar_month_day存入阴历1901年到2050年每年中的月天数信息，
-  #阴历每月只能是29或30天，一年用12（或13）个二进制位表示，对应位为1表30天，否则为29天
-  g_lunar_month_day = [
-    0x4ae0, 0xa570, 0x5268, 0xd260, 0xd950, 0x6aa8, 0x56a0, 0x9ad0, 0x4ae8, 0x4ae0,  #1910
-    0xa4d8, 0xa4d0, 0xd250, 0xd548, 0xb550, 0x56a0, 0x96d0, 0x95b0, 0x49b8, 0x49b0,  #1920
-    0xa4b0, 0xb258, 0x6a50, 0x6d40, 0xada8, 0x2b60, 0x9570, 0x4978, 0x4970, 0x64b0,  #1930
-    0xd4a0, 0xea50, 0x6d48, 0x5ad0, 0x2b60, 0x9370, 0x92e0, 0xc968, 0xc950, 0xd4a0,  #1940
-    0xda50, 0xb550, 0x56a0, 0xaad8, 0x25d0, 0x92d0, 0xc958, 0xa950, 0xb4a8, 0x6ca0,  #1950
-    0xb550, 0x55a8, 0x4da0, 0xa5b0, 0x52b8, 0x52b0, 0xa950, 0xe950, 0x6aa0, 0xad50,  #1960
-    0xab50, 0x4b60, 0xa570, 0xa570, 0x5260, 0xe930, 0xd950, 0x5aa8, 0x56a0, 0x96d0,  #1970
-    0x4ae8, 0x4ad0, 0xa4d0, 0xd268, 0xd250, 0xd528, 0xb540, 0xb6a0, 0x96d0, 0x95b0,  #1980
-    0x49b0, 0xa4b8, 0xa4b0, 0xb258, 0x6a50, 0x6d40, 0xada0, 0xab60, 0x9370, 0x4978,  #1990
-    0x4970, 0x64b0, 0x6a50, 0xea50, 0x6b28, 0x5ac0, 0xab60, 0x9368, 0x92e0, 0xc960,  #2000
-    0xd4a8, 0xd4a0, 0xda50, 0x5aa8, 0x56a0, 0xaad8, 0x25d0, 0x92d0, 0xc958, 0xa950,  #2010
-    0xb4a0, 0xb550, 0xb550, 0x55a8, 0x4ba0, 0xa5b0, 0x52b8, 0x52b0, 0xa930, 0x74a8,  #2020
-    0x6aa0, 0xad50, 0x4da8, 0x4b60, 0x9570, 0xa4e0, 0xd260, 0xe930, 0xd530, 0x5aa0,  #2030
-    0x6b50, 0x96d0, 0x4ae8, 0x4ad0, 0xa4d0, 0xd258, 0xd250, 0xd520, 0xdaa0, 0xb5a0,  #2040
-    0x56d0, 0x4ad8, 0x49b0, 0xa4b8, 0xa4b0, 0xaa50, 0xb528, 0x6d20, 0xada0, 0x55b0,  #2050
-  ]
-  #数组gLanarMonth存放阴历1901年到2050年闰月的月份，如没有则为0，每字节存两年
-  g_lunar_month = [
-    0x00, 0x50, 0x04, 0x00, 0x20,  #1910
-    0x60, 0x05, 0x00, 0x20, 0x70,  #1920
-    0x05, 0x00, 0x40, 0x02, 0x06,  #1930
-    0x00, 0x50, 0x03, 0x07, 0x00,  #1940
-    0x60, 0x04, 0x00, 0x20, 0x70,  #1950
-    0x05, 0x00, 0x30, 0x80, 0x06,  #1960
-    0x00, 0x40, 0x03, 0x07, 0x00,  #1970
-    0x50, 0x04, 0x08, 0x00, 0x60,  #1980
-    0x04, 0x0a, 0x00, 0x60, 0x05,  #1990
-    0x00, 0x30, 0x80, 0x05, 0x00,  #2000
-    0x40, 0x02, 0x07, 0x00, 0x50,  #2010
-    0x04, 0x09, 0x00, 0x60, 0x04,  #2020
-    0x00, 0x20, 0x60, 0x05, 0x00,  #2030
-    0x30, 0xb0, 0x06, 0x00, 0x50,  #2040
-    0x02, 0x07, 0x00, 0x50, 0x03  #2050
-  ]
-  START_YEAR = 1901
-  # 天干
-  gan = '甲乙丙丁戊己庚辛壬癸'
-  # 地支
-  zhi = '子丑寅卯辰巳午未申酉戌亥'
-  # 生肖
-  xiao = '鼠牛虎兔龙蛇马羊猴鸡狗猪'
-  # 月份
-  lm = '正二三四五六七八九十冬腊'
-  # 日份
-  ld = '初一初二初三初四初五初六初七初八初九初十十一十二十三十四十五十六十七十八十九二十廿一廿二廿三廿四廿五廿六廿七廿八廿九三十'
-  # 节气
-  jie = '小寒大寒立春雨水惊蛰春分清明谷雨立夏小满芒种夏至小暑大暑立秋处暑白露秋分寒露霜降立冬小雪大雪冬至'
-  def __init__(self, dt = None):
-    '''初始化：参数为datetime.datetime类实例，默认当前时间'''
-    self.localtime = dt if dt else datetime.datetime.today()
-  def sx_year(self): # 返回生肖年
-    ct = self.localtime #取当前时间
-    year = self.ln_year() - 3 - 1 # 农历年份减3 （说明：补减1）
-    year = year % 12 # 模12，得到地支数
-    return self.xiao[year]
-  def gz_year(self): # 返回干支纪年
-    ct = self.localtime #取当前时间
-    year = self.ln_year() - 3 - 1 # 农历年份减3 （说明：补减1）
-    G = year % 10 # 模10，得到天干数
-    Z = year % 12 # 模12，得到地支数
-    return self.gan[G] + self.zhi[Z]
-  def gz_month(self): # 返回干支纪月（未实现）
-    pass
-  def gz_day(self): # 返回干支纪日
-    ct = self.localtime #取当前时间
-    C = ct.year // 100 #取世纪数，减一
-    y = ct.year % 100 #取年份后两位（若为1月、2月则当前年份减一）
-    y = y - 1 if ct.month == 1 or ct.month == 2 else y
-    M = ct.month #取月份（若为1月、2月则分别按13、14来计算）
-    M = M + 12 if ct.month == 1 or ct.month == 2 else M
-    d = ct.day #取日数
-    i = 0 if ct.month % 2 == 1 else 6 #取i （奇数月i=0，偶数月i=6）
-    #下面两个是网上的公式
-    # http://baike.baidu.com/link?url=MbTKmhrTHTOAz735gi37tEtwd29zqE9GJ92cZQZd0X8uFO5XgmyMKQru6aetzcGadqekzKd3nZHVS99rewya6q
-    # 计算干（说明：补减1）
-    G = 4 * C + C // 4 + 5 * y + y // 4 + 3 * (M + 1) // 5 + d - 3 - 1
-    G = G % 10
-    # 计算支（说明：补减1）
-    Z = 8 * C + C // 4 + 5 * y + y // 4 + 3 * (M + 1) // 5 + d + 7 + i - 1
-    Z = Z % 12
-    #返回 干支纪日
-    return self.gan[G] + self.zhi[Z]
-  def gz_hour(self): # 返回干支纪时（时辰）
-    ct = self.localtime #取当前时间
-    #计算支
-    Z = round((ct.hour/2) + 0.1) % 12 # 之所以加0.1是因为round的bug!!
-    #返回 干支纪时（时辰）
-    return self.zhi[Z]
-  def ln_year(self): # 返回农历年
-    year, _, _ = self.ln_date()
-    return year
-  def ln_month(self): # 返回农历月
-    _, month, _ = self.ln_date()
-    return month
-  def ln_day(self): # 返回农历日
-    _, _, day = self.ln_date()
-    return day
-  def ln_date(self): # 返回农历日期整数元组（年、月、日）（查表法）
-    delta_days = self._date_diff()
-    #阳历1901年2月19日为阴历1901年正月初一
-    #阳历1901年1月1日到2月19日共有49天
-    if (delta_days < 49):
-      year = self.START_YEAR - 1
-      if (delta_days <19):
-       month = 11;
-       day = 11 + delta_days
-      else:
-        month = 12;
-        day = delta_days - 18
-      return (year, month, day)
-    #下面从阴历1901年正月初一算起
-    delta_days -= 49
-    year, month, day = self.START_YEAR, 1, 1
-    #计算年
-    tmp = self._lunar_year_days(year)
-    while delta_days >= tmp:
-      delta_days -= tmp
-      year += 1
-      tmp = self._lunar_year_days(year)
-    #计算月
-    (foo, tmp) = self._lunar_month_days(year, month)
-    while delta_days >= tmp:
-      delta_days -= tmp
-      if (month == self._get_leap_month(year)):
-        (tmp, foo) = self._lunar_month_days(year, month)
-        if (delta_days < tmp):
-          return (0, 0, 0)
-          return (year, month, delta_days+1)
-        delta_days -= tmp
-      month += 1
-      (foo, tmp) = self._lunar_month_days(year, month)
-    #计算日
-    day += delta_days
-    return (year, month, day)
-  def ln_date_str(self):# 返回农历日期字符串，形如：农历正月初九
-    _, month, day = self.ln_date()
-    return '农历{}月{}'.format(self.lm[month-1], self.ld[(day-1)*2:day*2])
-  def ln_jie(self): # 返回农历节气
-    ct = self.localtime #取当前时间
-    year = ct.year
-    for i in range(24):
-      #因为两个都是浮点数，不能用相等表示
-      delta = self._julian_day() - self._julian_day_of_ln_jie(year, i)
-      if -.5 <= delta <= .5:
-        return self.jie[i*2:(i+1)*2]
-    return ''
-  #显示日历
-  def calendar(self):
-    pass
-  #######################################################
-  #      下面皆为私有函数
-  #######################################################
-  def _date_diff(self):
-    '''返回基于1901/01/01日差数'''
-    return (self.localtime - datetime.datetime(1901, 1, 1)).days
-  def _get_leap_month(self, lunar_year):
-    flag = self.g_lunar_month[(lunar_year - self.START_YEAR) // 2]
-    if (lunar_year - self.START_YEAR) % 2:
-      return flag & 0x0f
+from time import localtime
+from requests import get, post
+from datetime import datetime, date
+from zhdate import ZhDate
+import sys
+import os
+
+
+def get_color():
+    # 获取随机颜色
+    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
+    color_list = get_colors(100)
+    return random.choice(color_list)
+
+
+def get_access_token():
+    # appId
+    app_id = config["app_id"]
+    # appSecret
+    app_secret = config["app_secret"]
+    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
+                .format(app_id, app_secret))
+    try:
+        access_token = get(post_url).json()['access_token']
+    except KeyError:
+        print("获取access_token失败，请检查app_id和app_secret是否正确")
+        os.system("pause")
+        sys.exit(1)
+    # print(access_token)
+    return access_token
+
+
+def get_weather(region):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+    }
+    key = config["weather_key"]
+    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
+    response = get(region_url, headers=headers).json()
+    if response["code"] == "404":
+        print("推送消息失败，请检查地区名是否有误！")
+        os.system("pause")
+        sys.exit(1)
+    elif response["code"] == "401":
+        print("推送消息失败，请检查和风天气key是否正确！")
+        os.system("pause")
+        sys.exit(1)
     else:
-      return flag >> 4
-  def _lunar_month_days(self, lunar_year, lunar_month):
-    if (lunar_year < self.START_YEAR):
-      return 30
-    high, low = 0, 29
-    iBit = 16 - lunar_month;
-    if (lunar_month > self._get_leap_month(lunar_year) and self._get_leap_month(lunar_year)):
-      iBit -= 1
-    if (self.g_lunar_month_day[lunar_year - self.START_YEAR] & (1 << iBit)):
-      low += 1
-    if (lunar_month == self._get_leap_month(lunar_year)):
-      if (self.g_lunar_month_day[lunar_year - self.START_YEAR] & (1 << (iBit -1))):
-         high = 30
-      else:
-         high = 29
-    return (high, low)
-  def _lunar_year_days(self, year):
-    days = 0
-    for i in range(1, 13):
-      (high, low) = self._lunar_month_days(year, i)
-      days += high
-      days += low
-    return days
-  # 返回指定公历日期的儒略日
-  def _julian_day(self):
-    ct = self.localtime #取当前时间
-    year = ct.year
-    month = ct.month
-    day = ct.day
-    if month <= 2:
-      month += 12
-      year -= 1
-    B = year / 100
-    B = 2 - B + year / 400
-    dd = day + 0.5000115740 #本日12:00后才是儒略日的开始(过一秒钟)*/
-    return int(365.25 * (year + 4716) + 0.01) + int(30.60001 * (month + 1)) + dd + B - 1524.5
-  # 返回指定年份的节气的儒略日数
-  def _julian_day_of_ln_jie(self, year, st):
-    s_stAccInfo =[
-       0.00, 1272494.40, 2548020.60, 3830143.80, 5120226.60, 6420865.80,
-       7732018.80, 9055272.60, 10388958.00, 11733065.40, 13084292.40, 14441592.00,
-       15800560.80, 17159347.20, 18513766.20, 19862002.20, 21201005.40, 22529659.80,
-       23846845.20, 25152606.00, 26447687.40, 27733451.40, 29011921.20, 30285477.60]
-    #已知1900年小寒时刻为1月6日02:05:00
-    base1900_SlightColdJD = 2415025.5868055555
-    if (st < 0) or (st > 24):
-      return 0.0
-    stJd = 365.24219878 * (year - 1900) + s_stAccInfo[st] / 86400.0
-    return base1900_SlightColdJD + stJd
-###############################################################
-# 获取当前时间
-today = datetime.now()
-# 获取变量设置的值
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
-user_id = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
-
-# 获取天气,【墨迹天气】
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']),weather['wind']
-
-# 计算开始到现在天数
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
-
-# 生日倒计时
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+        # 获取地区的location--id
+        location_id = response["location"][0]["id"]
+    weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
+    response = get(weather_url, headers=headers).json()
+    # 天气
+    weather = response["now"]["text"]
+    # 当前温度
+    temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    # 风向
+    wind_dir = response["now"]["windDir"]
+    return weather, temp, wind_dir
 
 
-# 获取彩虹屁语句
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+def get_birthday(birthday, year, today):
+    birthday_year = birthday.split("-")[0]
+    # 判断是否为农历生日
+    if birthday_year[0] == "r":
+        r_mouth = int(birthday.split("-")[1])
+        r_day = int(birthday.split("-")[2])
+        # 获取农历生日的今年对应的月和日
+        try:
+            birthday = ZhDate(year, r_mouth, r_day).to_datetime().date()
+        except TypeError:
+            print("请检查生日的日子是否在今年存在")
+            os.system("pause")
+            sys.exit(1)
+        birthday_month = birthday.month
+        birthday_day = birthday.day
+        # 今年生日
+        year_date = date(year, birthday_month, birthday_day)
 
-# 返回随机颜色
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    else:
+        # 获取国历生日的今年对应月和日
+        birthday_month = int(birthday.split("-")[1])
+        birthday_day = int(birthday.split("-")[2])
+        # 今年生日
+        year_date = date(year, birthday_month, birthday_day)
+    # 计算生日年份，如果还没过，按当年减，如果过了需要+1
+    if today > year_date:
+        if birthday_year[0] == "r":
+            # 获取农历明年生日的月和日
+            r_last_birthday = ZhDate((year + 1), r_mouth, r_day).to_datetime().date()
+            birth_date = date((year + 1), r_last_birthday.month, r_last_birthday.day)
+        else:
+            birth_date = date((year + 1), birthday_month, birthday_day)
+        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
+    elif today == year_date:
+        birth_day = 0
+    else:
+        birth_date = year_date
+        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
+    return birth_day
+
+
+def get_ciba():
+    url = "http://open.iciba.com/dsapi/"
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+    }
+    r = get(url, headers=headers)
+    note_en = r.json()["content"]
+    note_ch = r.json()["note"]
+    return note_ch, note_en
 
 
 
-# 微信公众号APPID和秘钥
-client = WeChatClient(app_id, app_secret)
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
+    week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+    year = localtime().tm_year
+    month = localtime().tm_mon
+    day = localtime().tm_mday
+    today = datetime.date(datetime(year=year, month=month, day=day))
+    week = week_list[today.isoweekday() % 7]
+    # 获取在一起的日子的日期格式
+    love_year = int(config["love_date"].split("-")[0])
+    love_month = int(config["love_date"].split("-")[1])
+    love_day = int(config["love_date"].split("-")[2])
+    love_date = date(love_year, love_month, love_day)
+    # 获取在一起的日期差
+    love_days = str(today.__sub__(love_date)).split(" ")[0]
+    # 获取所有生日数据
+    birthdays = {}
+    for k, v in config.items():
+        if k[0:5] == "birth":
+            birthdays[k] = v
+    data = {
+        "touser": to_user,
+        "template_id": config["template_id"],
+        "url": "http://weixin.qq.com/download",
+        "topcolor": "#FF0000",
+        "data": {
+            "date": {
+                "value": "{} {}".format(today, week),
+                "color": get_color()
+            },
+            "region": {
+                "value": region_name,
+                "color": get_color()
+            },
+            "weather": {
+                "value": weather,
+                "color": get_color()
+            },
+            "temp": {
+                "value": temp,
+                "color": get_color()
+            },
+            "wind_dir": {
+                "value": wind_dir,
+                "color": get_color()
+            },
+            "love_day": {
+                "value": love_days,
+                "color": get_color()
+            },
+            "note_en": {
+                "value": note_en,
+                "color": get_color()
+            },
+            "note_ch": {
+                "value": note_ch,
+                "color": get_color()
+            }
+        }
+    }
+    for key, value in birthdays.items():
+        # 获取距离下次生日的时间
+        birth_day = get_birthday(value["birthday"], year, today)
+        if birth_day == 0:
+            birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
+        else:
+            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
+        # 将生日数据插入data
+        data["data"][key] = {"value": birthday_data, "color": get_color()}
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+    }
+    response = post(url, headers=headers, json=data).json()
+    if response["errcode"] == 40037:
+        print("推送消息失败，请检查模板id是否正确")
+    elif response["errcode"] == 40036:
+        print("推送消息失败，请检查模板id是否为空")
+    elif response["errcode"] == 40003:
+        print("推送消息失败，请检查微信号是否正确")
+    elif response["errcode"] == 0:
+        print("推送消息成功")
+    else:
+        print(response)
 
-# 微信公众号消息
-wm = WeChatMessage(client)
-# 变量赋天气返回数据值 wea天气晴 temperature温度 wind风向
-wea, temperature, wind = get_weather()
-# 公众号设置返回消息
-data = {"humidity":{"value":city},"wind":{"value":wind},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-# 返回消息
-res = wm.send_template(user_id, template_id, data)
-# 打印消息
-print(res)
+
+if __name__ == "__main__":
+    try:
+        with open("config.txt", encoding="utf-8") as f:
+            config = eval(f.read())
+    except FileNotFoundError:
+        print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
+        os.system("pause")
+        sys.exit(1)
+    except SyntaxError:
+        print("推送消息失败，请检查配置文件格式是否正确")
+        os.system("pause")
+        sys.exit(1)
+
+    # 获取accessToken
+    accessToken = get_access_token()
+    # 接收的用户
+    users = config["user"]
+    # 传入地区获取天气信息
+    region = config["region"]
+    weather, temp, wind_dir = get_weather(region)
+    note_ch = config["note_ch"]
+    note_en = config["note_en"]
+    if note_ch == "" and note_en == "":
+        # 获取词霸每日金句
+        note_ch, note_en = get_ciba()
+    # 公众号推送消息
+    for user in users:
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+    os.system("pause")
